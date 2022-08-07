7abc06dc7c348b0ca6578dcca9b81c25f46def9c
ChungenAllen <chungen.chu@gmail.com>
2022-08-06 23:57:20 -0400

    5th new record

--- api/server.js
+++ api/server.js
@@ -7,7 +7,8 @@ const books = [
     {id: 1, title: 'Java and spring'},
     {id: 2, title: 'C# and .netframework'},
     {id: 3, title: 'php and laravel'},
-    {id: 4, title: 'node.js and express'}
+    {id: 4, title: 'node.js and express'},
+    {id: 5, title: 'ios book'}
 ];
 
 app.get('/api/books', (req, res) => {
