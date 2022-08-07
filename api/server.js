1ae863011d4b87497df6df3c5ab891bf4b7f75d0
ppigworm <ppigworm@yahoo.com>
2022-08-06 22:40:27 -0400

    first commit

---
+++ api/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const books = [
+    {id: 1, title: 'Java and spring'},
+    {id: 2, title: 'C# and .netframework'},
+    {id: 3, title: 'php and laravel'}
+];
+
+app.get('/api/books', (req, res) => {
+    res.send(books);
+});
+
+const port = process.env.port || 8080;
+app.listen(port, () => console.log(`Listening on port ${port}..`));
