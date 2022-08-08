96452a8ccfa63b17a0df0a668f9795da52c29d89
ChungenAllen <chungen.chu@gmail.com>
2022-08-08 14:25:19 -0400

    added props, event and css

---
+++ my-app/src/components/ToDoForm.vue
@@ -0,0 +1,27 @@
+<template>
+    <form @submit.prevent="onSubmit">
+        <label for="newtodoform">create a To DO item</label>
+        <input type="text" id="newtodoform" v-model.lazy.trim="newtask"/>
+         <button type="submit" class="btn btn__primary btn__lg">Add</button>
+    </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            newtask: "",
+        }
+    },
+
+    methods: {
+        onSubmit () {
+            if (this.newtask === "") {
+                return;
+            }
+            this.$emit('task-added', this.newtask);
+            this.newtask = "";
+        }
+    }
+}
+</script>
